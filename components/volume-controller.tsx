e7c1fdabbbca1a6726508f9d15824c7055b308b5
Exploser <exploserbusiness@gmail.com>
2024-08-03 12:28:14 -0400

    Implement volume controller component to allow users to dynamically control volume

---
+++ components/volume-controller.tsx
@@ -0,0 +1,28 @@
+import { useVolume } from "@/context/VolumeContext";
+
+const VolumeController: React.FC = () => {
+  const { volume, setVolume } = useVolume();
+
+  const handleVolumeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setVolume(Number(event.target.value));
+  };
+
+  return (
+    <div className=' sticky w-[20vw] bottom-0 right-0 flex flex-row items-center p-2 bg-black bg-opacity-40 text-white'>
+      <label className='whitespace-nowrap mx-2' htmlFor="volume">
+        Volume: {Math.round(volume * 100)}%
+      </label>
+      <input
+        id="volume"
+        type="range"
+        min="0"
+        max="1"
+        step="0.01"
+        value={volume}
+        onChange={handleVolumeChange}
+        className="w-full"
+      />
+    </div>
+  );
+}
+export default VolumeController;
