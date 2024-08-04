7351fd47e73bf7c881e669da98a6c9aecf4b227c
Exploser <exploserbusiness@gmail.com>
2024-08-04 14:42:45 -0400

    Fix warnings

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -52,12 +52,12 @@ const routes = [
 
 const Sidebar = () => {
     const pathname = usePathname();
-    return ( 
+    return (
         <div className="space-y-4 py-4 flex flex-col h-[100vh] bg-[#111827] text-white justify-between ">
             <div className="px-3 py-2 flex-1">
                 <Link href={"/"} className="flex items-center pl-3 mb-14">
                     <div className="relative w-8 h-8 mr-4">
-                        <Image src="/logo.svg" layout="fill" alt="Loser Logo"/>
+                        <Image src="/logo.svg" fill alt="Loser Logo" />
                     </div>
                     <h1 className={cn("text-2xl font-bold text-white", montserrat.className)}>
                         TunaTunes
@@ -65,17 +65,17 @@ const Sidebar = () => {
                 </Link>
                 <div className="space-y-1">
                     {routes.map((route, index) => (
-                            <Link key={index} href={route.href} className={cn("text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition", pathname===route.href ? "text-white bg-white/10" : 'text-zinc-400')}>
-                        <div className="flex items-center flex-1">
-                            <route.icon className={cn("w-6 h-6 mr-4", route.color)}/>
-                            {route.label}
-                        </div>
+                        <Link key={index} href={route.href} className={cn("text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition", pathname === route.href ? "text-white bg-white/10" : 'text-zinc-400')}>
+                            <div className="flex items-center flex-1">
+                                <route.icon className={cn("w-6 h-6 mr-4", route.color)} />
+                                {route.label}
+                            </div>
                         </Link>
                     ))}
                 </div>
             </div>
         </div>
-     );
+    );
 }
- 
+
 export default Sidebar;
